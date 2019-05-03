df2e563f616e14999869f11d564ffd5f7df0694a
liamcheney <liam.cheney1@gmail.com>
2019-05-03 14:27:11 +1000

    test commit

--- CTX_allele_caller/CTX_allele_caller.py
+++ CTX_allele_caller/CTX_allele_caller.py
@@ -1,29 +1,40 @@
 import argparse
+from os import path
+import sys
 from time import sleep as sl
 import subprocess
 from Bio.Blast.Applications import NcbiblastnCommandline
 
 
-def cerate_files_dicts():
-
-
-def parseargs():
+def parseargs(set_wd):
     parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 
-    parser.add_argument("-db", "--database_name", required=True,
+    parser.add_argument("-i", "--input_genome",
+                        help="Path to a single input genome (nucleotide) for analysis.")
+    parser.add_argument("-o", "--output_folder", default=set_wd + "/output/",
                         help="sql database to search (eg. vcseventh)")
+    parser.add_argument("-f", "--input_genomes_file",
+                        help="A comma separated list of genomes (nucleotide) to analyse.")
 
     args = parser.parse_args()
 
     return args
 
+def blast_input_against_db():
+
+    print()
+
 def main():
 
-    args = parseargs()
+    set_wd = path.dirname(path.abspath(__file__))
+    args = parseargs(set_wd)
+
+
 
 if __name__ == '__main__':
     main()
 
-#take in the files for both ctxb alleles and the query genome
-#build a blast database
+
+#blast against database
+#get results
 
