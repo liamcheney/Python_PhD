3c3a24af15b00331eada9c176cf12bfa22d3a976
liamcheney <Potle'2368>
2020-05-18 15:18:03 +1000

    test

--- Staph/2020-03-20-testing_typability.py
+++ Staph/2020-03-20-testing_typability.py
@@ -17,171 +17,10 @@ def parseargs():
 def main():
     args = parseargs()
 
-    # infiles = open("/Users/liamcheney/Desktop/all_ncbi_outputs.csv").read().split('\n\n')
-    #
-    # save_dict = {}
-    # save_list = []
-    # for file in infiles:
-    #     infile = file.split('\n')
-    #     for line in infile[1:]:
-    #         col = line.split(',')
-    #         gene = col[4]
-    #         if gene not in save_list:
-    #             save_list.append(gene)
-
-    # print(len(save_list))
-    # for i in save_list:
-    #     print(i)
-
-    # save_dict = {}
-    # for filename in glob.iglob('/Users/liamcheney/Desktop/outs/*_out.txt'):
-    #     infile = open(filename).read().splitlines()
-    #     acc = filename.split('/')[-1].split('_')[0]
-    #     intact = 0
-    #     partial = 0
-    #     called = 0
-    #     print(acc)
-    #
-    #     for line in infile:
-    #
-    #         if "intact:" in line:
-    #             intact = int(line.split(':')[-1].strip())
-    #         if "partial:" in line:
-    #             partial = int(line.split(':')[-1].strip())
-    #         if "called:" in line:
-    #             called = int(line.split(':')[-1].strip())
-    #
-    #     total_pass = intact + partial + called
-    #     failed =  1860 - total_pass
-    #     save_dict[acc] = {'total':total_pass, 'fail':failed}
-    #
-    # with open('/Users/liamcheney/Desktop/all_out11.csv','w') as out:
-    #     for k,v in save_dict.items():
-    #         out.write(k + ',')
-    #         for x in v:
-    #             out.write(str(save_dict[k][x]) + ',')
-    #         out.write('\n')
-
-
-    # ##check how gene went across dataset
-    # infile = open('//Users/liamcheneyy/Desktop/all_alleles.txt').read().splitlines()
-    # # print(len(infile))
-    #
-    # #make list of genes
-    # save_dict = {}
-    # for line in infile[0:1]:
-    #     col = line.split(' ')
-    #    # acc=col[0].split('/')[-1].split('_')[0]
-    #     for j in col[4:]:
-    #         gene = j.split(':')[0].strip('>')
-    #         save_dict[gene] = {'pass':0, 'fail':0}
-    #
-    # #iterate over
-    # for line in infile:
-    #     col = line.split(' ')
-    #
-    #     acc=col[0].split('/')[-1].split('_')[0]
-    #
-    #     for j in col[4:]:
-    #         gene = j.split(':')[0].strip('>')
-    #         if ':0' in j:
-    #             save_dict[gene]['fail'] = save_dict[gene]['fail'] + 1
-    #         # else:
-    #         #     save_dict[gene]['pass'] = save_dict[gene]['pass'] + 1
-    # #
-    # for k,v in save_dict.items():
-    #     print(k,v['pass'],v['fail'], sep='\t')
-    # #
-    # # ##check per strain zeros
-    # # save_dict = {}
-    # # for line in infile:
-    # #     col = line.split(' ')
-    # #
-    # #     acc=col[0].split('/')[-1].split('_')[0]
-    # #
-    # #     print(acc, line.count(':0'))
-
-    # ##count negs per gene
-    # path = "/Users/liamcheneyy/Desktop/all/"
-    #
-    # save_dict = {}
-    # for file in glob.iglob(path + "/*alleles.fasta"):
-    #     seqs = SeqIO.parse(file, 'fasta')
-    #     name=file.split('/')[-1].split('.')[0]
-    #     for records in seqs:
-    #
-    #         locus = records.id.split(':')[0]
-    #
-    #         if locus not in save_dict.keys():
-    #             save_dict[locus] = 0
-    #
-    #         if (':1' in records.id) or (':2' in records.id) or (':3' in records.id) or (':4' in records.id):
-    #             save_dict[locus] = save_dict[locus] + 1
-    #
-    # for k,v in save_dict.items():
-    #     print(k, v, sep='\t')
-
-# infile = open('/Users/liamcheneyy/Desktop/MLST_test.txt').read().splitlines()
-    #
-    # with open('/Users/liamcheneyy/Desktop/MLST_pass.txt','w') as out:
-    #     for line in infile:
-    #         col = line.split('\t')
-    #         if col[1] != '-':
-    #             if col[2] == '-':
-    #                 if ('(-)' not in line) and ('?' not in line):
-    #                     print(line)
-
-    # #FIX OUTPUTS TO REMOVE POOR ASSEMBLES
-    # infiles = open('/Users/liamcheneyy/Desktop/all_resfinder.csv').read().split('./')
-    # remove = open('/Users/liamcheneyy/Desktop/remove.txt').read().splitlines()
-    #
-    # with open('/Users/liamcheneyy/Desktop/90_all_alleles_out_fix.csv','w') as out:
-    #
-    #     for file in infiles[1:-1]:
-    #         infile = file.splitlines()
-    #         acc = infile[0].split('/')[-1].split('_')[0]
-    #         if acc not in remove:
-    #             out.write('./' + file)
-
-
-    # with open('/Users/liamcheneyy/Desktop/90_all_alleles_out_fix.csv','w') as out:
-    #     for line in infiles:
-    #         col = line.split(',')
-    #         acc=col[0].split('_')[0]
-    #         print(acc)
-    #         # if acc not in remove:
-    #         #     out.write(line + '\n')
-
-
-
-    # seqs = SeqIO.parse('/Users/liamcheneyy/Desktop/refs.fasta', 'fasta')
-    # save_list = []
-    # for records in seqs:
-    #
-    #     locus = records.id.split(':')[0]
-    #
-    #     if locus not in save_list:
-    #         save_list.append(locus)
-    #
-    # seqx = SeqIO.to_dict(SeqIO.parse('/Users/liamcheneyy/Desktop/refs.fasta', 'fasta'))
-    #
-    # save_dict = {}
-    # for el in save_list:
-    #     save_dict[el] = {}
-    #     for x in seqx:
-    #         if el in x:
-    #             save_dict[el][x] = seqx[x]
-    #
-    # for k,v in save_dict.items():
-    #     with open('/Users/liamcheneyy/Desktop/outs/' + k + '.fasta' ,'w') as out:
-    #         for i in v:
-    #             out.write('>' + i + '\n')
-    #             out.write(str(v[i].seq) + '\n')
-
-    ###go over allele profiles and get summary stats
+    ##go over allele profiles and get summary stats
     genome_zero_neg_dict = {}
     gene_zero_dict = {}
-    for filename in glob.iglob('/Users/liamcheneyy/Desktop/alleles/*fasta'):
+    for filename in glob.iglob('/Users/liamcheney/Desktop/alleles/*fasta'):
         bios = SeqIO.parse(filename,'fasta')
         accession = filename.split('/')[-1].split('_')[0]
         print(accession)
@@ -199,7 +38,7 @@ def main():
                 gene_zero_dict[gene]['zero'] = gene_zero_dict[gene]['zero'] + 1
                 genome_zero_neg_dict[accession]['zero'] = genome_zero_neg_dict[accession]['zero'] + 1
 
-            if ":1" in record.id or ":2" in record.id or ":3" in record.id or ":4" in record.id or ":5" in record.id or ":6" in record.id or ":7" in record.id:
+            if record.id.split(':')[-1].isdigit():
                 gene_zero_dict[gene]['intact'] = gene_zero_dict[gene]['intact'] + 1
 
             if ":new" in record.id:
@@ -210,7 +49,7 @@ def main():
                 genome_zero_neg_dict[accession]['neg'] = genome_zero_neg_dict[accession]['neg'] + 1
 
 
-    with open('/Users/liamcheneyy/Desktop/alleles/genome_zeros.txt', 'w') as out:
+    with open('/Users/liamcheney/Desktop/alleles/genome_zeros.txt', 'w') as out:
         out.write('Accessions' + '\t' + 'Zero' + '\t' + 'Neg' + '\n')
         for key, value in genome_zero_neg_dict.items():
             out.write(key + '\t')
@@ -218,7 +57,7 @@ def main():
                 out.write(str(value[i]) + '\t')
             out.write('\n')
 
-    with open('/Users/liamcheneyy/Desktop/alleles/gene_zeros.txt', 'w') as out:
+    with open('/Users/liamcheney/Desktop/alleles/gene_zeros.txt', 'w') as out:
         out.write('Gene' + '\t' + 'Zero' + '\t' + 'Intact' + '\t' + 'New' + '\t' + 'Neg' + '\n')
         for key, value in gene_zero_dict.items():
             out.write(key + '\t')
@@ -226,10 +65,28 @@ def main():
                 out.write(str(value[i]) + '\t')
             out.write('\n')
 
-
-
-
-        # genome_zero_dict[accession] = genome_zero_conut
+    # ##remove locus from alleles based on input list
+    # infile = open('/Users/liamcheney/Desktop/xx.txt','r').read().splitlines()
+    # save_dict  = {}
+    # for filename in glob.iglob('/Users/liamcheney/Desktop/alleles/*fasta'):
+    #     records = SeqIO.parse(filename, 'fasta')
+    #
+    #     accession = filename.split('/')[-1]
+    #     save_dict[accession] = {}
+    #     for record in records:
+    #         if not record.id.split(':')[0].strip('>') in infile:
+    #             save_dict[accession][record.id] = record.seq
+    #
+    # for k, v in save_dict.items():
+    #     print(k)
+    #     with open('/Users/liamcheney/Desktop/fixed/' + k ,'w') as out:
+    #         for id,seq in v.items():
+    #             out.write(">" + id + "\n")
+    #
+    #             if "new" in id:
+    #                 out.write(str(seq) + "\n\n")
+    #             elif not "new" in id:
+    #                 out.write(str(seq) + "\n")
 
 if __name__ == '__main__':
     main()
